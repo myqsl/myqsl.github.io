d7983852b9b209d7c3843f76257f47cea87749e0
Eugene Kornykhin <shortwavesaregreat@gmail.com>
2023-06-23 16:43:28 +0300

    Bot was updated.

--- py/bot.py
+++ py/bot.py
@@ -8,26 +8,26 @@ from telegram.ext import ApplicationBuilder, filters, MessageHandler, ContextTyp
 
 async def forwarder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.channel_post
-    if msg:
-        # print(msg, flush=True)
-        # print(msg.text, flush=True)
-        # print(msg.date.ctime() + " " + msg.text, flush=True)
-        text = escape(msg.text)
-        text = sub(r'(https://youtu.be/[0-9a-zA-Z/]*)', r'<a href="\1">\1</a>', text) 
-        line = f'<p><code>{msg.date.strftime("%B %d")}</code>&nbsp;{text}</p>'
+    if not msg: return
+    
+    text = escape(msg.text)
+    text = sub(r'(https://youtu.be/[0-9a-zA-Z/]*)', r'<a href="\1">\1</a>', text) 
+    line = f'<p><code>{msg.date.strftime("%B %d")}</code>&nbsp;{text}</p>'
 
-        feed = Path('feed.txt')
-        lines = feed.read_text().splitlines()
-        lines.insert(0, line)
-        feed.write_text('\n'.join(lines))
+    feed = Path('feed.txt')
+    lines = feed.read_text().splitlines()
+    lines.insert(0, line)
+    feed.write_text('\n'.join(lines))
 
-        html = Path('../_includes/updates.html')
-        html.write_text('\n'.join(lines[:5]))
+    html = Path('../_includes/updates.html')
+    html.write_text('\n'.join(lines[:5]))
 
-        # TODO git pull, add, merge, push
-        repo = Repo('..')
-        repo.remotes.origin.pull()
-        repo.index.add(['.'])
+    repo = Repo('..')
+    repo.remotes.origin.pull()
+    repo.index.add(['py/feed.txt', '_includes/updates.html'])
+    repo.index.commit("myqsl_github_io: updates were added.")
+    repo.remotes.origin.push()
+
 
 application = ApplicationBuilder().token(KEY).build()
 forwardHandler = MessageHandler(filters.TEXT & (~filters.COMMAND), forwarder)

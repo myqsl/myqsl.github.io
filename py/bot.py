e5563c65a4b42141709e99f81b16e32b9de816e0
Eugene Kornykhin <shortwavesaregreat@gmail.com>
2023-07-08 00:42:05 +0300

    Strong and Emph were added to pybot.

--- py/bot.py
+++ py/bot.py
@@ -12,6 +12,8 @@ async def forwarder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     text = escape(msg.text)
     text = sub(r'(https://youtu.be/[0-9a-zA-Z/_]*)', r'<a href="\1">\1</a>', text)
+    text = sub(r'\*\*(.*?)\*\*', r'<strong>\1</strong>', text)
+    text = sub(r'__(.*?)__', r'<em>\1</em>', text)
     line = f'<p><code>{msg.date.strftime("%B %d")}</code>&nbsp;{text}</p>'
 
     feed = Path('feed.txt')
